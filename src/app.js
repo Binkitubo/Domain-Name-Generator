b43d4bb7b5ce4b9e529bc373d34ff3945d775ffb
Binkitubo <davidlolleria@hotmail.com>
2022-07-08 19:13:45 +0000

    Loops para dominio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".us", ".io"];
+
+  const domain = (param1, param2, param3, param4) => {
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+          for (let e = 0; e < extension.length; e++) {
+            let resultado = `${param1[i]}${param2[a]}${param3[n]}${param4[e]}`;
+            console.log(resultado);
+          }
+        }
+      }
+    }
+  };
+  domain(pronoun, adj, noun, extension);
 };
